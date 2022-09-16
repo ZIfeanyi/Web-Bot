374404eb80a11bfbe8548e4122d50df56df3708d
Ifeanyi <113346969+CoderIfeanyi@users.noreply.github.com>
2022-09-16 16:09:30 +0100

    Update Web-Bot.py

--- Web-Bot.py
+++ Web-Bot.py
@@ -4,7 +4,7 @@ choice=input("(yes(1)/no(2)): ")
 if choice == '1':
     webbrowser.open("https://github.com/CoderIfeanyi")
 else:
-    activity=input("What would you like to open:\nGoogle(1):\nYoutube(2):\nGitHub(3):\nCalculator(4):\n ")
+    activity=input("\nWhat would you like to open:\nGoogle(1):\nYoutube(2):\nGitHub(3):\nCalculator(4):\nMusic(5):\nSpeech Recognition(6):\n ")
 if activity == '1':
                webbrowser.open("https://google.com")
 if activity == '2':
@@ -15,5 +15,7 @@ if activity == '3':
 
 if activity == '4':
     webbrowser.open("https://www.online-calculator.com/full-screen-calculator/")
-    
-    
+
+if activity == '5':
+    webbrowser.open("https://open.spotify.com/")
+
